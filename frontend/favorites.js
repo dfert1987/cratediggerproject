90361dd37df1c756356d174be64f4b850751aa6b
dfert1987 <github email address>
2020-05-29 13:06:28 -0600

    fixed submit buttons on front page. favorites page is properly stylizzed

--- frontend/favorites.js
+++ frontend/favorites.js
@@ -11,16 +11,17 @@ function parse(artists) {
 }
 
 function createArtistCard(artist) {
-    const artistDiv = document.createElement('container')
+    const artistDiv = document.createElement('div')
     const artistName = document.createElement('h3')
     const artistImage = document.createElement('div')
+    const flexBox = document.getElementById('flexbox')
 
 
     artistName.innerHTML= `<a href=http://localhost:3001/showartist.html?id=${artist.id}>${artist.name}</a> `
-    
     artistImage.innerHTML = `<img src="${artist.image}"/>`
+    artistDiv.className ='container'
 
     artistDiv.append(artistName, artistImage)
-    document.body.append(artistDiv)
+    flexBox.append(artistDiv)
 
 }
